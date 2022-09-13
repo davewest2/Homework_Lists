80c6a66568a513759d4a95aa72dae20fa840561b
Dave West <davewest2@hotmail.co.uk>
2022-09-13 16:10:23 +0100

    Implemented and tested Ordered Queue with new Class "Car" sorted by colour (String)

--- src/main/kotlin/OrderedQueueTest.kt
+++ src/main/kotlin/OrderedQueueTest.kt
@@ -2,17 +2,17 @@ import junit.framework.TestCase.*
 import org.junit.Test
 
 class Car(val year: Int, val make: String, val colour: String)
-val myCar1 = Car(1950, "Ford","Blue" )
-val myCar2 = Car(1970, "Datsun", "Red")
+val myCar1 = Car(1950, "Cadillac","Pink" )
+val myCar2 = Car(1970, "Datsun", "Cherry")
 val myCar3 = Car(1985, "DeLorean", "Silver")
 val myCar4 = Car(1930, "Ford", "Black")
 val myCar5 = Car(1960, "Porsche", "White")
 
 class OrderedQueueTest {
     val numberOrdering: (a: Car, b: Car) -> Int = { a, b ->
-        if (a.year < b.year) {
+        if (a.colour < b.colour) {
             1
-        } else if (a.year == b.year) {
+        } else if (a.colour == b.colour) {
             0
         } else {
             -1
@@ -27,7 +27,8 @@ class OrderedQueueTest {
         arr.addItemToOrderedQueue(myCar3)
         arr.addItemToOrderedQueue(myCar4)
         arr.addItemToOrderedQueue(myCar5)
-        assertEquals(arr.toList(), listOf(myCar4, myCar1, myCar5, myCar2, myCar3))
+        assertEquals(arr.toList(), listOf(myCar4, myCar2, myCar1, myCar3,
+            myCar5))
     }
 
     @Test
@@ -37,7 +38,7 @@ class OrderedQueueTest {
         arr.addItemToOrderedQueue(myCar2)
         arr.addItemToOrderedQueue(myCar3)
         val removed = arr.removeFromQueue()
-        assertEquals(removed, myCar1)
+        assertEquals(removed, myCar2)
     }
 
     @Test
@@ -58,7 +59,7 @@ class OrderedQueueTest {
         arr.addItemToOrderedQueue(myCar2)
         arr.addItemToOrderedQueue(myCar3)
         val firstItem = arr.peek()
-        assertEquals(firstItem, myCar1)
+        assertEquals(firstItem, myCar2)
     }
 
     @Test
